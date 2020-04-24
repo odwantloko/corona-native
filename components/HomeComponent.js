5a91cd9834a1968e28eeb7ae0841c005e51368fc
ntlokoo <Odwa.Ntloko@vodacom.co.za>
2020-04-24 12:11:09 +0200

    added more functionality and components to home

--- components/HomeComponent.js
+++ components/HomeComponent.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
+import SimpleMap from './MapComponent';
 
 export default class Home extends Component {
   
@@ -13,8 +14,10 @@ export default class Home extends Component {
         lat: -29.087217,
         lng: 26.154898
       }, 
-      zoom: 6.2
-		}
+      zoom: 6.2,
+      date: null
+    }
+   
   }
   
   componentDidMount = () => {
@@ -31,19 +34,22 @@ export default class Home extends Component {
     fetch('https://api.covid19api.com/live/country/south-africa/status/confirmed', postOptions)
         .then(res => res.json())
         .then((data) => {
-		this.setState({data:data[data.length-1]})
+        data[data.length-1].Date =new Date(data[data.length-1].Date).toLocaleString(); 
 
+    this.setState({data:data[data.length-1]})
+  
 
     }).catch(console.log())
 
-    // // Global Stats API Call
-    // fetch('https://api.covid19api.com/summary', postOptions)
-    //     .then(res => res.json())
-    //     .then((data) => {
-    //     this.setState({global_data:data.Global})
-    //     console.log(data.Global)   
-    //     }).catch(console.log)
+    // Global Stats API Call
+    fetch('https://api.covid19api.com/summary', postOptions)
+        .then(res => res.json())
+        .then((data) => {
+        this.setState({global_data:data.Global})
+        this.setState({date:new Date(data.Date).toLocaleString()})
 
+        }).catch(console.log)
+      
   
   }
   
@@ -51,15 +57,51 @@ export default class Home extends Component {
   render() {
     return (
     <View>
-        <Text>Confirmed Cases: {this.state.data.Confirmed} </Text>
-        <Text>Active Cases: {this.state.data.Active} </Text>
-        <Text>Recovered Cases: {this.state.data.Recovered} </Text>
-        <Text>Deaths: {this.state.data.Deaths} </Text>
-    </View>
-     
-       
-      
+        <View>  
+          <Text style={styles.headings}> South African Stats</Text>
+          <View style={{flexDirection: 'row'}}>
+            <Text style={styles.textDefault}>Confirmed Cases: </Text><Text style={styles.textConfirmed}>{this.state.data.Confirmed} </Text>
+            <Text style={styles.textDefault}>Active Cases: </Text><Text style={styles.textActive}>{this.state.data.Active} </Text>
+          </View>
+          <View style={{flexDirection: 'row'}}>
+            <Text style={styles.textDefault}>Recovered Cases: </Text><Text style={styles.textRecovered}>{this.state.data.Recovered} </Text>
+            <Text style={styles.textDefault}>Deaths: </Text><Text style={styles.textDeaths}>{this.state.data.Deaths} </Text>
+          </View>
+          <View style={{flexDirection: 'row'}}>
+            <Text style={styles.textUpdatedDate}> Last Updated at {this.state.data.Date} </Text>
+          </View>
+      </View>
+
+      <SimpleMap/>
+
+      <View>
+        <Text style={styles.headings}>Global Stats </Text>
+        <View style={{flexDirection: 'row'}}>
+          <Text style={styles.textDefault}>Confirmed Cases: </Text><Text style={styles.textConfirmed}>{this.state.global_data.TotalConfirmed} </Text>
+        </View>
+        <View style={{flexDirection: 'row'}}>
+          <Text style={styles.textDefault}>Recovered Cases: </Text><Text style={styles.textRecovered}>{this.state.global_data.TotalRecovered} </Text>
+          <Text style={styles.textDefault}>Deaths: </Text><Text style={styles.textDeaths}>{this.state.global_data.TotalDeaths} </Text>
+        </View>
+        <View style={{flexDirection: 'row'}}>
+          <Text style={styles.textUpdatedDate}> Last Updated at {this.state.date} </Text>
+        </View>
+      </View>
+    </View> 
     );
   }
 };
+
+const styles = StyleSheet.create({
+  textDefault : { textAlign: 'center', fontWeight: '600', color:'#a9a9a9', fontSize:18, paddingBottom: 10},
+  textConfirmed: { textAlign: 'center', fontWeight: '600', color:'#ffce52', fontSize:18},
+  textActive: { textAlign: 'center', fontWeight: '600', color:'#52b7ff', fontSize:18},
+  textRecovered: { textAlign: 'center', fontWeight: '600', color:'#72ff52',fontSize:18 },
+  textDeaths: { textAlign: 'center', fontWeight: '600', color:'#ff5852',fontSize:18 },
+  textUpdatedDate: { textAlign: 'right', fontWeight: '400', color:'#a9a9a9',fontSize:12 },
+  headings:  { textAlign: 'center', fontWeight: '700', color:'#a9a9a9',fontSize:22, paddingBottom:20, paddingTop:10}
+
+
+});
+
 

e4ab31deee84913d21269055fd2f4267fcae31b1
ntlokoo <Odwa.Ntloko@vodacom.co.za>
2020-04-28 11:53:24 +0200

    Added Google Map component

--- components/MapComponent.js
+++ components/MapComponent.js
@@ -1,9 +1,7 @@
 import React, { Component } from 'react';
 // import GoogleMapReact from 'google-map-react';
 import { Text, View } from 'react-native';
-import MapView from 'react-native-maps';
-
-
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
 
 export default class SimpleMap extends Component {
   static defaultProps = {
@@ -18,27 +16,18 @@ export default class SimpleMap extends Component {
  
   render() {
     return (
-    //   <View style={{ height: '95vh', width: '100%' }}>
-    //   <GoogleMapReact
-    //     bootstrapURLKeys={{ key: "AIzaSyDoATwXhVPVR-k3VQCzhac4QZmXkYxfxRI" }}
-    //     defaultCenter={this.props.center}
-    //     defaultZoom={this.props.zoom}   
-    //   >
-      
-    //   </GoogleMapReact>
-    //   </View>
-    <View style={{ height: '95%', width: '100%' }}>
-        <MapView
-            initialRegion={{
-            latitude: 37.78825,
-            longitude: -122.4324,
-            latitudeDelta: 0.0922,
-            longitudeDelta: 0.0421,
-            }}
-        />
-    </View>
-
-
+      <MapView
+        style={{ flex: 1, height: 340}}
+        provider={PROVIDER_GOOGLE}
+        showsUserLocation
+        initialRegion={{
+        latitude: -29.087217,
+        longitude: 26.154898,
+        latitudeDelta: 15,
+        longitudeDelta: 19}}
+        // latitudeDelta: 0.0922,
+        // longitudeDelta: 0.0421}}
+    />
     );
   }
 }
